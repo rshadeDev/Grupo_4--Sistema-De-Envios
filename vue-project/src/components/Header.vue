c15318879fbe0d7d70b432d6646783583dfd9c79
Benjamin San Martin <b.sanmartin06@ufromail.cl>
2024-05-01 19:48:39 -0400

    creacion componentes Header y SectionIconSucursal

---
+++ vue-project/src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+    <header>
+        <img src="../../public/img/Logo.png" alt="logo">
+
+        <div>
+            <span>Rastrear Pedido</span>
+            <input type="text" placeholder="Ingresa tu N° de orden">
+
+            <button>Ingresar</button>
+            <!-- Agregar los botones si es que esta ingresada una cuenta, o si es que no hay.
+                
+            <button v-if="isAuthenticated">Cuenta</button>
+            <button v-else>Registrarse</button>
+            -->
+        </div>
+    </header>
+</template>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.7rem 2rem;
+    background-color: rgb(238, 100, 50);
+}
+
+img {
+    width: 11%;
+}
+
+div {
+    display: flex;
+    align-items: center;
+    margin-right: 10px;
+}
+
+span {
+    margin-right: 10px;
+    color: white;
+    font-weight: 800;
+    width: 37%;
+}
+
+input {
+    padding: 0.5rem;
+    border: none;
+    width: 50%;
+    border-radius: 7px;
+}
+
+button {
+    width: 80px;
+    margin-left: 10px;
+    padding: 0.5rem;
+    border: none;
+    border-radius: 7px;
+    background-color: #FFEAD4;
+    color: orangered;
+    cursor: pointer;
+}</style>
---
+++ vue-project/src/components/SectionIconSucursal.vue
@@ -0,0 +1,47 @@
+<template>
+    <section>
+        <div>
+            <a href="#"><img src="../../public/img/icono-cotizacion.png" alt="Icono cotizacion" class="img-normalizada"></a>
+            <a href="#"><img src="../../public/img/icono-sucursal.png" alt="Icono sucursal" class="img-normalizada"></a>
+            <a href="#"><img src="../../public/img/icono-seguimiento.png" alt="Icono seguimiento"
+                    class="img-normalizada"></a>
+        </div>
+    </section>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 6em;
+}
+
+a {
+    text-decoration: none;
+}
+
+div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+img {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 6em;
+    margin-left: 6.5em;
+
+    border: 5px solid #FF6D24;
+    border-radius: 40%;
+}
+
+.img-normalizada {
+    width: 120px;
+    height: 120px;
+}</style>
