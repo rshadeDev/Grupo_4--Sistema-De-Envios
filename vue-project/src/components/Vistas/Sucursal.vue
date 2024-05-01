e89efd6ea37f04c39909c2ed0635463e8eb6b641
Benjamin San Martin <b.sanmartin06@ufromail.cl>
2024-05-01 19:51:33 -0400

    creacion vista sucursal

---
+++ vue-project/src/components/Vistas/Sucursal.vue
@@ -0,0 +1,11 @@
+<template>
+    <Header />
+    <SectionIconSucursal />
+    <SectionSearchSucursal />
+</template>
+
+<script setup>
+import Header from '../Header.vue';
+import SectionIconSucursal from '../SectionIconSucursal.vue';
+import SectionSearchSucursal from '../SectionSearchSucursal.vue';
+</script>
