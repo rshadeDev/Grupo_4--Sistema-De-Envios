088f65f4aea1914e9bd0138b442cd07ba9993e85
Benjamin San Martin <b.sanmartin06@ufromail.cl>
2024-05-14 10:46:36 -0400

    update SectionSearchSucursal.vue

--- vue-project/src/components/SectionSearchSucursal.vue
+++ vue-project/src/components/SectionSearchSucursal.vue
@@ -4,24 +4,34 @@
         <h3>Ingresa la direccion o ciudad en la que busques una sucursal</h3>
 
         <div class="search-bar">
-            <input type="text" placeholder="Ingresa tu N° de orden">
-            <a href="#">
-                <button>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="14" viewBox="0 0 24 20" fill="none"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                        class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-right">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M5 12l14 0" />
-                        <path d="M15 16l4 -4" />
-                        <path d="M15 8l4 4" />
-                    </svg>
-                </button>
-            </a>
+            <input type="text" placeholder="Ingresa la ciudad" v-model="city">
+            <button @click="buscarSucursales">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="14" viewBox="0 0 24 20" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-right">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M5 12l14 0" />
+                    <path d="M15 16l4 -4" />
+                    <path d="M15 8l4 4" />
+                </svg>
+            </button>
         </div>
     </section>
 </template>
 
-<script setup>
+<script>
+export default {
+    data() {
+        return {
+            city: ''
+        };
+    },
+    methods: {
+        buscarSucursales() {
+            this.$router.push({ path: '/lista-sucursal', query: { city: this.city } });
+        }
+    }
+};
 </script>
 
 <style scoped>
