e875cf9ebdf2075064cfb1783604f5f8ff4c0d0d
Benjamin San Martin <b.sanmartin06@ufromail.cl>
2024-05-14 10:44:15 -0400

    update main.js

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Componentes a cargar
+import Sucursal from './components/Vistas/Sucursal.vue'
+import ListaSucursal from './components/Vistas/ListaSucursal.vue'
+import Login from './components/Vistas/Login.vue'
+
+// definir rutas
+const routes = [
+    { path: '/', component: Sucursal },
+    { path: '/lista-sucursal', component: ListaSucursal },
+    { path: '/login', component: Login }
+]
+
+// Crear el router
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+createApp(App).use(router).mount('#app')
