56b89e0565d7a21301d3cddd2a3a673e09dd6dde
Byersen <h.garrido02@ufromail.cl>
2024-05-20 04:22:53 -0400

    import InicioSesion y Registro

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import App from './App.vue'
 import Sucursal from './components/Vistas/Sucursal.vue'
 import ListaSucursal from './components/Vistas/ListaSucursal.vue'
 
+import Registro from './components/Vistas/Registro.vue'
+import InicioSesion from './components/Vistas/InicioSesion.vue'
+
 import Seguimiento from './components/Vistas/Seguimiento.vue'
 import InfoSeguimiento from './components/Vistas/InfoSeguimiento.vue'
 import MapaSeguimiento from './components/Vistas/MapaSeguimiento.vue'
@@ -15,6 +18,8 @@ import MapaSeguimiento from './components/Vistas/MapaSeguimiento.vue'
 const routes = [
     { path: '/sucursal', component: Sucursal },
     { path: '/lista-sucursal', component: ListaSucursal },
+    { path: '/registro', component: Registro },
+    { path: '/inicio-sesion', component: InicioSesion },
     { path: '/seguimiento', component: Seguimiento},
     { path: '/info-seguimiento/:pedidoRecibido',name:"info-pedido", component: InfoSeguimiento, props: true},
     { path: '/info-seguimiento/mapa-seguimiento', component: MapaSeguimiento}
