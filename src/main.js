d1330783199f4bf0111881a017df087ec3ccaceb
Byersen <h.garrido02@ufromail.cl>
2024-05-20 21:51:10 -0400

    import vistas cotizaciones

--- src/main.js
+++ src/main.js
@@ -10,6 +10,11 @@ import ListaSucursal from './components/Vistas/ListaSucursal.vue'
 import Registro from './components/Vistas/Registro.vue'
 import InicioSesion from './components/Vistas/InicioSesion.vue'
 
+import Cotizacion from './components/Vistas/Cotizacion.vue'
+import Cotizacion2Tipo from './components/Vistas/Cotizacion2Tipo.vue'
+import Cotizacion3Document from './components/Vistas/Cotizacion3Document.vue'
+import Cotizacion4Paquete from './components/Vistas/Cotizacion4Paquete.vue'
+
 import Seguimiento from './components/Vistas/Seguimiento.vue'
 import InfoSeguimiento from './components/Vistas/InfoSeguimiento.vue'
 import MapaSeguimiento from './components/Vistas/MapaSeguimiento.vue'
@@ -20,6 +25,10 @@ const routes = [
     { path: '/lista-sucursal', component: ListaSucursal },
     { path: '/registro', component: Registro },
     { path: '/inicio-sesion', component: InicioSesion },
+    { path: '/cotizacion', component: Cotizacion },
+    { path: '/cotizacion2-tipo', component: Cotizacion2Tipo },
+    { path: '/cotizacion3-document', component: Cotizacion3Document },
+    { path: '/cotizacion4-paquete', component: Cotizacion4Paquete },
     { path: '/seguimiento', component: Seguimiento},
     { path: '/info-seguimiento/:pedidoRecibido',name:"info-pedido", component: InfoSeguimiento, props: true},
     { path: '/info-seguimiento/mapa-seguimiento/:pedidoRecibido',name:"mapa-pedido", component: MapaSeguimiento, props:true}
